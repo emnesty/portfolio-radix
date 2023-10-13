cf9eb56c9a28bc0a88d3f52308d1fb17a3adf626
Luciano Silva <legostr@gmail.com>
2023-10-13 01:49:31 -0300

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,32 +12,30 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR">
       <Head>
-        <meta charSet="UTF-8" />
         <title>Luciano Silva UX/UI Designer - Front-End Developer</title>
+        <meta name="title" content="Luciano Silva UX/UI Designer - Front-End Developer" />
         <meta
           name="description"
           content="Olá, sou Luciano Silva, um UX/UI Designer e Desenvolvedor Frontend com experiência em grandes empresas de tecnologia. Neste portfólio, você poderá explorar meus trabalhos criativos, recomendações e habilidades adquiridas ao longo da minha jornada profissional. Sinta-se à vontade para se conectar comigo no LinkedIn e explorar meus projetos no Github para ter uma visão mais aprofundada do meu expertise técnico e criativo."
         />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.lucianosilva.dev/" />
         <meta property="og:title" content="Luciano Silva UX/UI Designer - Front-End Developer" />
         <meta
           property="og:description"
           content="Olá, sou Luciano Silva, um UX/UI Designer e Desenvolvedor Frontend com experiência em grandes empresas de tecnologia. Neste portfólio, você poderá explorar meus trabalhos criativos, recomendações e habilidades adquiridas ao longo da minha jornada profissional. Sinta-se à vontade para se conectar comigo no LinkedIn e explorar meus projetos no Github para ter uma visão mais aprofundada do meu expertise técnico e criativo."
         />
-        <meta property="og:url" content="https://lucianosilva.dev" />
-        <meta property="og:image" content="https://lucianosilva.dev/share-og.jpg" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@lucianosilva.dev" />
-        <meta name="twitter:creator" content="@LockerzLBR" />
-        <meta name="twitter:title" content="Luciano Silva UX/UI Designer - Front-End Developer" />
+        <meta property="og:image" content="https://lucianosilva.dev/images/opengraph-image.jpg" />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.lucianosilva.dev/" />
+        <meta property="twitter:title" content="Luciano Silva UX/UI Designer - Front-End Developer" />
         <meta
-          name="twitter:description"
+          property="twitter:description"
           content="Olá, sou Luciano Silva, um UX/UI Designer e Desenvolvedor Frontend com experiência em grandes empresas de tecnologia. Neste portfólio, você poderá explorar meus trabalhos criativos, recomendações e habilidades adquiridas ao longo da minha jornada profissional. Sinta-se à vontade para se conectar comigo no LinkedIn e explorar meus projetos no Github para ter uma visão mais aprofundada do meu expertise técnico e criativo."
         />
-        <meta name="twitter:image" content="https://lucianosilva.dev/opengraph-image.jpg" />
-        <link rel="canonical" href="https://lucianosilva.dev" />
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#171916" />
+        <meta property="twitter:image" content="https://lucianosilva.dev/images/opengraph-image.jpg" />
       </Head>
       <body className={inter.className}>
         <AOSInit />
