a132026643d743b1b47394118cf50cf51c3aba7b
Luciano Silva <legostr@gmail.com>
2023-10-13 20:12:49 -0300

    Ajustes header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -22,11 +22,11 @@ const Header = () => {
               </div>
               <NavigationMenu>
                 <NavigationMenuList className="flex gap-5">
-                  <NavigationMenuItem className="hover:bg-slate-800 focus:shadow-slate-800 group flex  items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
-                    <Link href="/">{translations.experience}</Link>
-                  </NavigationMenuItem>
                   <NavigationMenuItem className="hover:bg-slate-800 focus:shadow-slate-800 group flex  items-center justify-between gap-[2px] rounded-[4px] px-2 py-2 text-[16px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
                     <Link href="/">{translations.about}</Link>
+                  </NavigationMenuItem>
+                  <NavigationMenuItem className="hover:bg-slate-800 focus:shadow-slate-800 group flex  items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
+                    <Link href="/">{translations.experience}</Link>
                   </NavigationMenuItem>
                   <NavigationMenuItem className="hover:bg-slate-800 focus:shadow-slate-800 group flex  items-center justify-between gap-[2px] rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none outline-none focus:shadow-[0_0_0_2px]">
                     <Link href="/">{translations.cases}</Link>
