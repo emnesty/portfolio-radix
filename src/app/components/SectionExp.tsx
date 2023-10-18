86ff8b3ee7d9617df3af563eeaeed0e09916b3b9
Luciano Silva <legostr@gmail.com>
2023-10-18 16:11:46 -0300

    Animações

--- src/app/components/SectionExp.tsx
+++ src/app/components/SectionExp.tsx
@@ -4,14 +4,14 @@ import React from "react"
 const ExpSection = () => {
   return (
     <Container style={{ marginLeft: 15, marginRight: 15 }}>
-      <div data-aos="fade-left" data-aos-duration="900">
+      <div data-aos="fade-right" data-aos-duration="1100">
         <Heading>Experiência</Heading>
       </div>
-      <div data-aos="fade-left" data-aos-duration="800" className="max-w-[500px] mb-9">
+      <div data-aos="fade-right" data-aos-duration="1000" className="max-w-[500px] mb-9">
         <Text>Minha trajetória profissional ao longo dos anos</Text>
       </div>
       <div className="-my-6">
-        <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div data-aos="fade-up" data-aos-duration="900" className="relative pl-8 sm:pl-32 py-6 group">
           <Flex>
             <Link size="6" weight="bold" href="https://www.ambevtech.com.br/" target="_blank">
               AmbevTech
@@ -36,7 +36,7 @@ const ExpSection = () => {
             apresentando descobertas e resultados de pesquisas à equipe.
           </Text>
         </div>
-        <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div data-aos="fade-up" data-aos-duration="700" className="relative pl-8 sm:pl-32 py-6 group">
           <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-[100%] before:px-px before:bg-[#FFA057] sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-[#F76B15] after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
             <Badge className="sm:absolute left-0 inline-flex items-center justify-center font-semibold uppercase  mb-3 sm:mb-0">
               Nov 2020
@@ -54,7 +54,7 @@ const ExpSection = () => {
             diferentes setores para assegurar a continuidade operacional e a satisfação do cliente.
           </Text>
         </div>
-        <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div data-aos="fade-up" data-aos-duration="600" className="relative pl-8 sm:pl-32 py-6 group">
           <div className="flex flex-col sm:flex-row items-start mb-1 group-last:before:hidden before:absolute before:left-2 sm:before:left-0 before:h-[300px] before:px-px before:bg-[#FFA057] sm:before:ml-[6.5rem] before:self-start before:-translate-x-1/2 before:translate-y-3 after:absolute after:left-2 sm:after:left-0 after:w-2 after:h-2 after:bg-[#F76B15] after:border-4 after:box-content after:border-slate-50 after:rounded-full sm:after:ml-[6.5rem] after:-translate-x-1/2 after:translate-y-1.5">
             <Badge className="sm:absolute left-0 inline-flex items-center justify-center font-semibold uppercase  mb-3 sm:mb-0">
               Out 2015
@@ -72,7 +72,7 @@ const ExpSection = () => {
             garantindo uma operação fluida dos sistemas.
           </Text>
         </div>
-        <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div data-aos="fade-up" data-aos-duration="500" className="relative pl-8 sm:pl-32 py-6 group">
           <Link size="6" weight="bold" href="https://www.linx.com.br/" target="_blank">
             Linx
           </Link>
@@ -93,7 +93,7 @@ const ExpSection = () => {
             Desenvolvimento, atendimento de chamados por chat ou telefone.
           </Text>
         </div>
-        <div className="relative pl-8 sm:pl-32 py-6 group">
+        <div data-aos="fade-up" data-aos-duration="450" className="relative pl-8 sm:pl-32 py-6 group">
           <Link size="6" weight="bold" href="https://www.t-systems.com/br/pt" target="_blank">
             T-Systems
           </Link>
--- src/app/components/SectionWorks.tsx
+++ src/app/components/SectionWorks.tsx
@@ -27,46 +27,52 @@ const SectionWorks = () => {
   return (
     <Section>
       <Container className="mr-[15px] ml-[15px]">
-        <Heading>Trabalhos e Cases</Heading>
-        <Text>Alguns dos meus trabalhos e cases enquanto Designer e Desenvolvedor Frontend.</Text>
-        <Grid
-          columns="3"
-          width="auto"
-          gap="4"
-          mt="8"
-          className="max-xl:grid-cols-2 max-xl:grid max-md:grid-cols-1">
-          {projetosData.map((projeto, index) => (
-            <Link href={`/projetos/${projeto.id}`} key={projeto.id}>
-              <Card key={index} size="2" style={{ width: 350 }}>
-                {" "}
-                {/*projeto={projeto} */}
-                <Inset side="top" pb="current">
-                  <Image
-                    style={{ borderRadius: 8 }}
-                    src={projeto.imgsrc}
-                    width={350}
-                    height={350}
-                    alt={projeto.title}
-                  />
-                </Inset>
-                <Flex direction="column" gap="2">
-                  <Flex gap="2">
-                    {projeto.badges.map((badge: string, index: number) => (
-                      <Badge key={index} size="1" className="max-w-full">
-                        {badge}
-                      </Badge>
-                    ))}
+        <div data-aos="fade-right" data-aos-duration="1100">
+          <Heading>Trabalhos e Cases</Heading>
+        </div>
+        <div data-aos="fade-right" data-aos-duration="1000">
+          <Text>Alguns dos meus trabalhos e cases enquanto Designer e Desenvolvedor Frontend.</Text>
+        </div>
+        <div data-aos="fade-right" data-aos-duration="900">
+          <Grid
+            columns="3"
+            width="auto"
+            gap="4"
+            mt="8"
+            className="max-xl:grid-cols-2 max-xl:grid max-md:grid-cols-1">
+            {projetosData.map((projeto, index) => (
+              <Link href={`/projetos/${projeto.id}`} key={projeto.id}>
+                <Card key={index} size="2" style={{ width: 350 }}>
+                  {" "}
+                  {/*projeto={projeto} */}
+                  <Inset side="top" pb="current">
+                    <Image
+                      style={{ borderRadius: 8 }}
+                      src={projeto.imgsrc}
+                      width={350}
+                      height={350}
+                      alt={projeto.title}
+                    />
+                  </Inset>
+                  <Flex direction="column" gap="2">
+                    <Flex gap="2">
+                      {projeto.badges.map((badge: string, index: number) => (
+                        <Badge key={index} size="1" className="max-w-full">
+                          {badge}
+                        </Badge>
+                      ))}
+                    </Flex>
+                    <Text as="p" size="3">
+                      <Strong>{projeto.title}</Strong> {/* {projeto.description} */}
+                    </Text>
+                    <Button variant="outline">{translations.view}</Button>
+                    <Flex gap="2"></Flex>
                   </Flex>
-                  <Text as="p" size="3">
-                    <Strong>{projeto.title}</Strong> {/* {projeto.description} */}
-                  </Text>
-                  <Button variant="outline">{translations.view}</Button>
-                  <Flex gap="2"></Flex>
-                </Flex>
-              </Card>
-            </Link>
-          ))}
-        </Grid>
+                </Card>
+              </Link>
+            ))}
+          </Grid>
+        </div>
       </Container>
     </Section>
   )
