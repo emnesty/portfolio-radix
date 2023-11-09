37273a214679680c1650417c44e76823a9737c15
Luciano Silva <legostr@gmail.com>
2023-11-08 21:12:41 -0300

    Ajuste footer

--- src/app/components/SectionFooter.tsx
+++ src/app/components/SectionFooter.tsx
@@ -3,7 +3,7 @@ import React from "react"
 
 const SectionFooter = () => {
   return (
-    <footer data-aos="fade-up" className="bg-neutral-800 rounded-lg shadow m-4 dark:bg-gray-800">
+    <footer className="bg-neutral-800 rounded-lg shadow m-4 dark:bg-gray-800">
       <Container>
         <div className="w-full flex items-center justify-center mx-auto max-w-screen-xl p-4 md:flex md:items-center">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
